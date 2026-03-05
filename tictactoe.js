25d672fce983327c882fd1a218a94157ddb06385
mohammadasif-27 <mohammad.asif2005a@gmail.com>
2026-03-05 11:26:32 +0530

    add features

--- tictactoe.js
+++ tictactoe.js
@@ -1,2 +1,24 @@
 let boxes = document.querySelectorAll(".box");
 let resert = document.querySelector("#resert");
+let turn0 = true;
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,6],
+    [6,7,8],
+];
+boxes.forEach((box) => {
+    box.addEventListener("clicke",() =>{
+        console.log("box was clicked");
+        if(turn0){
+            box.innerText = "0";
+            turn0=false;
+        }else{
+            box.innerText="X";
+        }
+    });
+});
