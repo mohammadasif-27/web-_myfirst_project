36ce16d79c5cd790e9b1e7f8e5410bc13fad575c
mohammadasif-27 <mohammad.asif2005a@gmail.com>
2026-03-06 16:20:47 +0530

    access new btn & msg container

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resert = document.querySelector("#resert");
+let newGamebtn = document.querySelector("#new-btn");
+let msgcontainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 let turn0 = true;
 const winPatterns = [
     [0,1,2],
@@ -8,17 +11,46 @@ const winPatterns = [
     [1,4,7],
     [2,5,8],
     [2,4,6],
-    [3,4,6],
+    [3,4,5],
     [6,7,8],
 ];
-boxes.forEach((box) => {
-    box.addEventListener("clicke",() =>{
+boxes.forEach((box) =>{
+    box.addEventListener("click", () => {
         console.log("box was clicked");
         if(turn0){
-            box.innerText = "0";
+            box.innerText = "X";
             turn0=false;
         }else{
-            box.innerText="X";
+            box.innerText="0";
+            turn0=true;
         }
+        box.disabled = true;
+       checkwinner();
     });
 });
+const showWinner = (winner) =>{
+    msg.innerText='congratulations';
+    msgcontainer.classList.remove("hide");
+}
+ 
+    const checkwinner = () => {
+         for ( let pattern of winPatterns){
+            // console.log([pattern[0]],[pattern[1]],[pattern[2]]);
+            // console.log(
+            //     boxes[pattern[0]].innerText,
+            //     boxes[pattern[1]].innerText,
+            //     boxes[pattern[2]].innerText
+            // );
+            let pos1Val = boxes[pattern[0]].innerText;
+            let pos2Val = boxes[pattern[1]].innerText;
+            let pos3Val = boxes[pattern[2]].innerText;
+            if(pos1Val !="" && pos2Val !="" && pos3Val !=""){
+                if(pos1Val === pos2Val && pos2Val === pos3Val){
+                    console.log("winner");
+                    showWinner();
+                }
+
+            }
+
+            }
+    };
